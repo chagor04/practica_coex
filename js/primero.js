8152ff47d6c616d33f174bb7d2ccb0ed2ab55b5a
johan sebastian <johan.sarmiento@nsb.edu.co>
2023-02-10 09:32:43 -0500

    js

---
+++ js/primero.js
@@ -0,0 +1,84 @@
+//ejercicio 1
+//var nume1=parseInt(prompt("ingrese el primer numero"))
+//var nume2=parseInt(prompt("ingrese el segundo nuemro"))
+//var nume3=parseInt(prompt("ingrese el tercer numero"))
+
+//let u=Math.max(nume1,nume2,nume3);
+//let a=Math.min(nume1,nume2,nume3);
+
+//if(nume1!=nume2!=nume3){
+//    console.log(u)
+//    console.log(a)
+//}else{
+//    window.alert("hola")
+//}
+
+//ejercicio 2
+//var base= parseInt(prompt("dijite la base en cm"))
+//var altura= parseInt(prompt("dijite la altura en cm"))
+//
+//if (base==altura){
+//    if(base>5 && altura>4){
+//        const base2=base*2+10
+//        let op2 =base2*altura/2
+//        console.log("el area el triangulo es"*op2)
+//    }
+//}else{
+//    let op =base*altura/2
+//    console.log("el area el triangulo es"*op)
+//}
+
+//ejercicio 3
+//var numero=parseInt(prompt("dijite un numero"))
+//a=numero%2
+//? es un operador ternario 
+//a===0 ? console.log("es par"):console.log("es inpar")
+
+//ejercicio 4
+//var nota = parseInt(prompt("diga su nota"))
+//var a = (19,20)
+//var b = (16,17,18)
+//var c = (13,14,15)
+//var d = (10,11,12)
+//var e =(1,2,3,4,5,6,7,8,9)
+
+//if (nota===a){
+//    console.log("usted saco A")
+//}else if(nota===b){
+//    console.log("usted saco B")
+//}else if(nota===c){
+//    console.log("usted saco C")
+//}else if(nota===d){
+//    console.log("usted saco D")
+//}else if(nota===e){
+//    console.log("usted saco e")
+//}else{
+//    console.log("hermano pa q estudia")
+//}
+
+//ejercicio 5
+//var n_1=parseInt(prompt("digite un numero"))
+//var n_2=parseInt(prompt("digite un numero"))
+
+//var n_f= n_1% n_2
+ 
+//if (n_f===0){
+//    console.log("son multiplos")
+//}else{
+//    console.log("no son multiplos")
+//}
+
+// ejercicio 6 
+//var numero=parseInt(prompt("dijite un numero"))
+
+//if (numero>0){
+ //   console.log("es negativo")
+//}else if(numero=0){
+//    console.log("el numero es 0")
+//}else{
+//    console.log("es positivo")
+//}
+
+//ejercicio 7
+
+
